68b010fc684218e22299106070fb2a39e7af78ad
Sergey Trofimov <sergey@trofimov.name>
2022-12-20 09:41:03 +0100

    Prepare project base

---
+++ README.md
@@ -0,0 +1,8 @@
+# undertow-ring
+
+Clojure ring adapter to Undertow web server.
+
+[![cljdoc badge](https://cljdoc.org/badge/com.github.strojure/undertow-ring)](https://cljdoc.org/d/com.github.strojure/undertow-ring)
+[![Clojars Project](https://img.shields.io/clojars/v/com.github.strojure/undertow-ring.svg)](https://clojars.org/com.github.strojure/undertow-ring)
+
+## Motivation
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.github.strojure/undertow-ring "1.0.0-SNAPSHOT"
+  :description "Clojure ring adapter to Undertow web server."
+  :url "https://github.com/strojure/undertow-ring"
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies []
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}}
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]])
