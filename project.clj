57912bbc080bd3760f7f4d447ae89724d02231a7
Sergey Trofimov <sergey@trofimov.name>
2023-04-27 14:59:30 +0200

    (chore): bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/ring-undertow "1.2.0-109"
+(defproject com.github.strojure/ring-undertow "1.2.1-110-SNAPSHOT"
   :description "Clojure ring adapter to Undertow web server."
   :url "https://github.com/strojure/ring-undertow"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
